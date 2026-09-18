b2dd3e2e8c49b7b06ed659c2eed02b4f38ed492f
hemantwint-hey <hemantwint@gmail.com>
2026-09-18 20:21:58 +0530

    Added the global exception handler

--- src/main/java/org/example/notificationservice/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/notificationservice/exception/GlobalExceptionHandler.java
@@ -2,14 +2,22 @@ package org.example.notificationservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    public ProblemDetail handleInvalidRequest(InvalidNotificationRequestException ex){
+    @ExceptionHandler(InvalidNotificationRequestException.class)
+    public ProblemDetail handleInvalidRequest(InvalidNotificationRequestException ex) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Request validation failed");
-
+        problem.setTitle("Invalid notification request");
+        problem.setProperty("errors", ex.getViolations());
+        return problem;
+    }
+    @ExceptionHandler(NotificationNotFoundException.class)
+    public ProblemDetail handleNotFound(NotificationNotFoundException ex) {
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problem.setTitle("Notification not found");
+        return problem;
     }
 }
